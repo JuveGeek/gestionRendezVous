d42b4535d4666fbfd0e1cbeb9962fcf3b2eeff48
JuveGeek <piljuvenal@gmail.com>
2022-09-14 03:43:07 +0200

    deuxieme commitll

--- src/main/java/gestion/pkg/gestionRendezVous/model/Clinique.java
+++ src/main/java/gestion/pkg/gestionRendezVous/model/Clinique.java
@@ -8,14 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity(name="clinique")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Clinique implements Serializable{
 	
 	@Id 
